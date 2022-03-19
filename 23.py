00af3c0097265412ab6b2ce700cdea2027e3beb1
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-19 11:44:40 +0900

    [Finish]やや難

--- 23.py
+++ 23.py
@@ -7,16 +7,14 @@
 体重を記録したN日間のうち、高橋くんがベストボディーであった日数を求めてください。
 """
 
-n=int(input("体重を記録した日数を入力してください："))
-s=int(input("ベストボディーの下限体重を入力してください"))
-t=int(input("ベストボディーの上限体重を入力してください："))
-x=int(input("１日目の高橋くんの体重を入力してください："))
-
-list_weight=[]
-
-for i in range(n):
-    a=int(input("i日目の体重を入力してください："))
-    list_weight.append(a)
-    a=a+1
-
-print(list_weight)
+n,s,t = map(int,input().split())
+w = int(input())
+cnt=0
+if s<=w<=t:
+    cnt+=1
+for i in range(n-1):
+    w=int(input())
+    if s<=w<=t:
+        cnt+=1
+    
+print(cnt)
