9a0b3e7a3a4a9ea6968b8bf829a427a14d716b3d
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-20 22:07:27 +0900

    [Finish]なんとなく納得出ません

---
+++ B7.py
@@ -0,0 +1,22 @@
+#辞書式順列
+"""
+import random
+
+A = str(input())
+if A == 'a':
+    print(-1)
+
+else:
+    B = ''.join(random.choices('abcdefghijklmnopqrstuvwxyz'))
+ここまでしかできなかった。
+ランダムな文字列を作成する方法がわからなかった。
+また文字列の長さがちょうど良くなるま文字列生成を繰り返す部分がうまくできなかった。
+"""
+
+A =input()
+
+if A == 'a':
+    print(-1)
+else:
+    print('a')
+    
