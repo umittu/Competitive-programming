3035282da3b45f56fc8e1c373cd536f62b59cbe3
海津宏基 <kaidzuhiroki@kaidzuhirokinoMacBook-Air.local>
2022-03-21 23:00:28 +0900

    [Finish]難ビット演算？

--- B14.py
+++ B14.py
@@ -1,23 +1,7 @@
 #価格の計算
-"""
-#商品数(n)の入力
-n = int(input("商品数を入力；"))
-#部分集合の(X)入力
-X = int(input("商品の部分集合くを入力；"))
-#部分集合(X)を2進数に変換
-def base10int(value, base):
-    if (int(value / base)):
-        return base10int(int(value / base), base) + str(value % base)
-    return str(value % base)
-
-Y = (base10int(X, 2))
-
-Z = str(Y)
-if len(Z) != n:
-    W = '0'+Z 
-else:
-    W = Z
-
-print(W)
-諦めた
-"""
+n,x = map(int,input().split())
+a = list(map(int,input().split()))
+y = 0 
+for i in range(n):
+    if x >> i & 1: ans +=a[i]
+print(y)
